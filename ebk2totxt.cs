a83a2b756260b98f36855ddc18e205669777baf4
root <root@FWD_YF_145_202.(none)>
2016-08-31 17:58:18 +0800

    add the test case.

---
+++ ebk2totxt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ebk2totxt
+{
+    class ebk2totxt
+    {
+        static void Main(string[] args)
+        {
+            const string book_name = "c:\\chaoyuepingyong.ebk2";
+            
+            StreamReader sr = new StreamReader(book_name);
+
+            //while(!sr.EndOfStream)
+            //{
+            //    string temp = sr.ReadLine();
+            
+            //    if(temp != null)
+            //    {
+            //        Console.WriteLine(temp);
+            //    }
+            //}
+
+            string array = sr.ReadToEnd();
+
+        }
+    }
+}
